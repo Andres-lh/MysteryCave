c56d954f08b7a09dd0723a68382a7e26de3f4b8c
Andres Lara <andres-lh@outlook.com>
2022-10-27 22:22:37 -0500

    refactor to add time loops

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -7,8 +7,18 @@ public class TimeController : MonoBehaviour
 {
     [Header("Timer Setup")]
     [SerializeField] private TextMeshProUGUI timerDisplay;
+    [SerializeField] private TextMeshProUGUI loopText;
     [SerializeField] private float timeInSeconds;
-    // Update is called once per frame
+
+    private float currentTime;
+    private int timeLoop = 0;
+    public bool shouldCountDown = true;
+    
+    private void Start()
+    {
+        currentTime = timeInSeconds;
+    }
+
     void Update()
     {
         Timer();
@@ -16,15 +26,28 @@ public class TimeController : MonoBehaviour
 
     private void Timer()
     {
-        timeInSeconds -= Time.deltaTime;
 
-        if (timeInSeconds <= 0)
+        if(currentTime >= 0 && shouldCountDown) currentTime -= Time.deltaTime;
+
+        if (currentTime <= 0 && shouldCountDown) 
         {
-            timeInSeconds = 0;
+            shouldCountDown = false;
+            currentTime = 0;
             timerDisplay.color = Color.red;
+            
+            StartCoroutine(RestartCountDown());
         }
 
-        timerDisplay.text = timeInSeconds.ToString("0.0");
-    } 
-
+        timerDisplay.text = currentTime.ToString("0.0");
+    }
+    
+    private IEnumerator RestartCountDown()
+    {
+        timeLoop += 1;
+        yield return new WaitForSeconds(2);
+        currentTime = timeInSeconds;
+        loopText.text = $"Time loop: {timeLoop}";
+        timerDisplay.color = Color.white;
+        shouldCountDown = true;
+    }
 }
