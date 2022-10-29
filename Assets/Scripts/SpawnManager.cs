2936b94579bd1d50b5a02e2ac84faf761510a44d
Andres Lara <andres-lh@outlook.com>
2022-10-29 16:06:41 -0500

    spawn objects in a random position

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private List<BoxCollider2D> rooms;
+    [SerializeField] private List<GameObject> spawnObject;
+    //  [SerializeField] private List<GameObject> targets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateObjects();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void CreateObjects()
+    {
+        foreach (GameObject i in spawnObject)
+        {
+            int randomRoom = Random.Range(0, rooms.Count);
+            Vector3 point = RandomPointInBounds(rooms[randomRoom].bounds);
+            Instantiate(i, point, i.transform.rotation);
+        }
+    }
+
+    private static Vector3 RandomPointInBounds(Bounds bounds)
+    {
+        return new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            Random.Range(bounds.min.y, bounds.max.y),
+            0
+        );
+    }
+}
